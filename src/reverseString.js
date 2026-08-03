fc49744e78cfb8b68ac44f0298e3056c458faf12
Canberk <canberkg98@hotmail.com>
2026-08-03 16:45:51 +0300

    feat: add reverseString function

---
+++ src/reverseString.js
@@ -0,0 +1,6 @@
+export function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+//reverse and join methods are array methods.
+//we split the string so that we can have an array.
